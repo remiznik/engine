cbf781346980ac6ed31e319aa84d1a2877616070
remiznik <remiznik@gmail.com>
2020-02-23 15:08:46 +0300

    fix problem with include

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,20 @@
-#include <SFML/Graphics.hpp>
-
-
 #include "script_system/script.h"
+#include "graphics/scene.h"
+#include "graphics/rectangle.h"
 
-#include "scene.h"
-#include "rectangle.h"
 #include "fps_lock.h"
-
 #include <memory>
 
 int  main()
 {
 	script_system::Script script;
-
 	engine::Scene scene;
+
 	auto rectangle = std::make_shared<engine::Rectangle>();
 	scene.addShape(rectangle);		
 	
-	sf::Clock clock;
 	float lastTime = 0;
 	utils::FpsLock lock(20);
-	sf::Font font;
-	if (!font.loadFromFile("../arial.ttf"))
-	{
-		//throw;
-	}
 
 	while (true)
 	{
