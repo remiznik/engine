1f03ea6f2ca11dba6b6fba0a56c9c9ceb432b276
remiznik <remiznik@gmail.ocm>
2020-05-06 23:23:48 +0300

    draw simple box

--- src/application.cpp
+++ src/application.cpp
@@ -239,10 +239,8 @@ namespace app {
 				update();
 				draw();
 			}
-			else
-			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			}
+			
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));			
 
 			
 		}
