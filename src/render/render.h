7d027bc13bba10c794cd8570678d446ef4a82c59
remiznik <remiznik@gmail.ocm>
2020-05-10 22:44:24 +0300

    add coder from example Shape

--- src/render/render.h
+++ src/render/render.h
@@ -8,25 +8,53 @@
 #include "d3d12Utils/d3dUtil.h"
 #include "d3d12Utils/UploadBuffer.h"
 #include "d3d12Utils/MathHelper.h"
+#include "d3d12Utils/FrameResource.h"
 
 // Link necessary d3d12 libraries.
 #pragma comment(lib,"d3dcompiler.lib")
 #pragma comment(lib, "D3D12.lib")
 #pragma comment(lib, "dxgi.lib")
 
+
+
 namespace render
 {
-	
-	struct ObjectConstants
-	{
-		DirectX::XMFLOAT4X4	WorldViewProj = MathHelper::Identity4x4();		
-	};
-
+		
 	struct Vertex
 	{
 		DirectX::XMFLOAT3 Pos;
 		DirectX::XMFLOAT4 Color;
 	};
+
+	struct RenderItem
+	{
+		RenderItem() = default;
+
+		// World matrix of the shape that describes the object's local space
+		// relative to the world space, which defines the position, orientation,
+		// and scale of the object in the world.
+		DirectX::XMFLOAT4X4 World = MathHelper::Identity4x4();
+
+		// Dirty flag indicating the object data has changed and we need to update the constant buffer.
+		// Because we have an object cbuffer for each FrameResource, we have to apply the
+		// update to each FrameResource.  Thus, when we modify obect data we should set 
+		// NumFramesDirty = gNumFrameResources so that each frame resource gets the update.
+		int NumFramesDirty = gNumFrameResources;
+
+		// Index into GPU constant buffer corresponding to the ObjectCB for this render item.
+		UINT ObjCBIndex = -1;
+
+		MeshGeometry* Geo = nullptr;
+
+		// Primitive topology.
+		D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+		// DrawIndexedInstanced parameters.
+		UINT IndexCount = 0;
+		UINT StartIndexLocation = 0;
+		int BaseVertexLocation = 0;
+	};
+
 
 
 	class RenderD12
@@ -38,6 +66,8 @@ namespace render
 		void draw();
 
 		void resize();
+		void updateCamera(float radius, float phi, float theta);
+		void update();
 
 
 	private:
@@ -47,12 +77,20 @@ namespace render
 		void CreateSwapChain();
 		void CreateRtvAndDsvDescriptorHeaps();
 
-		void BuildDescriptorHeaps();
-		void BuildConstantBuffers();
+
 		void BuildRootSignature();
 		void BuildShadersAndInputLayout();
-		void BuildBoxGeometry();
-		void BuildPSO();
+		void BuildShapeGeometry();
+		void BuildRenderItems();
+		void BuildFrameResources();
+		void BuildDescriptorHeaps();
+		void BuildConstantBufferViews();
+		void BuildPSOs();
+
+		void UpdateObjectCBs();
+		void UpdateMainPassCB();
+
+		void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
 
 		D3D12_CPU_DESCRIPTOR_HANDLE DepthStencilView()const;
 		ID3D12Resource* CurrentBackBuffer() const;
@@ -62,61 +100,75 @@ namespace render
 		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> RtvHeap_;
 		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> DsvHeap_;
 
-		Microsoft::WRL::ComPtr<ID3D12CommandQueue> CommandQueue_;
+		Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue_;
 		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> DirectCmdListAlloc_;
-		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList_;
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList_;
 
 		Microsoft::WRL::ComPtr<IDXGIFactory4> dxgiFactory_;
-		Microsoft::WRL::ComPtr<IDXGISwapChain> SwapChain_;
+		Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain_;
 		Microsoft::WRL::ComPtr<ID3D12Device> d3dDevice_;
 
 		Microsoft::WRL::ComPtr<ID3D12Fence> fence_;
 
-		D3D12_VIEWPORT ScreenViewport_;
-		D3D12_RECT ScissorRect_;
+		D3D12_VIEWPORT screenViewport_;
+		D3D12_RECT scissorRect_;
 
+		UINT passCbvOffset_ = 0;
 		UINT RtvDescriptorSize_ = 0;
 		UINT DsvDescriptorSize_ = 0;
-		UINT CbvSrvUavDescriptorSize_ = 0;
-		DXGI_FORMAT DepthStencilFormat_ = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		UINT cbvSrvUavDescriptorSize_ = 0;
 
-		DXGI_FORMAT BackBufferFormat_ = DXGI_FORMAT_R8G8B8A8_UNORM;
+		DXGI_FORMAT depthStencilFormat_ = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		DXGI_FORMAT backBufferFormat_ = DXGI_FORMAT_R8G8B8A8_UNORM;
+		
 
-		// Set true to use 4X MSAA (§4.1.8).  The default is false.
+		// Set true to use 4X MSAA
 		bool      _4xMsaaState = false;    // 4X MSAA enabled
 		UINT      _4xMsaaQuality = 0;      // quality level of 4X MSAA
 
 		static const int SwapChainBufferCount = 2;
-		int CurrBackBuffer_ = 0;
+		int currBackBuffer_ = 0;
 		Microsoft::WRL::ComPtr<ID3D12Resource> SwapChainBuffer_[SwapChainBufferCount];
 		Microsoft::WRL::ComPtr<ID3D12Resource> DepthStencilBuffer_;
 
-		UINT64 CurrentFence_ = 0;
-		Microsoft::WRL::ComPtr<ID3D12PipelineState> PSO_ = nullptr;
-		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CbvHeap_{ nullptr };
-		Microsoft::WRL::ComPtr<ID3D12RootSignature> RootSignature_{ nullptr };
+		UINT64 currentFence_ = 0;		
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> cbvHeap_{ nullptr };
+		Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_{ nullptr };
 
 		std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB_{ nullptr };
 
 		std::unique_ptr<MeshGeometry> BoxGeo_ = nullptr;
-		std::vector<D3D12_INPUT_ELEMENT_DESC> InputLayout_;
+		std::vector<D3D12_INPUT_ELEMENT_DESC> inputLayout_;
 		Microsoft::WRL::ComPtr<ID3DBlob> vsByteCode_ = nullptr;
 		Microsoft::WRL::ComPtr<ID3DBlob> psByteCode_ = nullptr;
 
 
+		std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> geometries_;
+		std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3DBlob>> shaders_;
+		std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3D12PipelineState>> PSOs_;
+
 		// from window 
 		HWND      hMainWnd_ = nullptr; // main window handle
-		int ClientWidth_ = 800;
-		int ClientHeight_ = 600;
+		int clientWidth_ = 800;
+		int clientHeight_ = 600;
 
 		// camera
 		DirectX::XMFLOAT4X4 mWorld = MathHelper::Identity4x4();
 		DirectX::XMFLOAT4X4 mView = MathHelper::Identity4x4();
-		DirectX::XMFLOAT4X4 mProj = MathHelper::Identity4x4();
+		DirectX::XMFLOAT3 eyePos_ = { 0.0f, 0.0f, 0.0f };
+		
 
-		float mTheta = 1.5f * DirectX::XM_PI;
-		float mPhi = DirectX::XM_PIDIV4;
-		float mRadius = 5.0f;
+		DirectX::XMFLOAT4X4 mProj = MathHelper::Identity4x4();		
+		std::vector<std::unique_ptr<FrameResource>> frameResources_;
+		FrameResource* currFrameResource_ = nullptr;
+		int currFrameResourceIndex_ = 0;
+		
+		// List of all the render items.
+		std::vector<std::unique_ptr<RenderItem>> allRitems_;
+		// Render items divided by PSO.
+		std::vector<RenderItem*> opaqueRitems_;
+
+		PassConstants mainPassCB_;
 	};
 
 }
