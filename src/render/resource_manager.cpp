e2abafb8f8ef34824b1c892d61859d1692c6eadf
remiznik <remiznik@gmail.ocm>
2020-05-15 18:35:00 +0300

    get data from file

--- src/render/resource_manager.cpp
+++ src/render/resource_manager.cpp
@@ -1,6 +1,8 @@
 #include "resource_manager.h"
 
+#include "core/m_math.h"
 #include <iostream>
+#include <sstream>
 
 namespace render {
 		
@@ -29,7 +31,38 @@ namespace render {
 	{
 		auto fileName = "../res/Models/" + name;
 		auto s = fileSystem_.readFile(fileName);
-		std::cout << s << std::endl;
+		std::stringstream  stream(s);
+
+		string ss;
+		int vertexCount;
+		int triangleCount;
+		stream >> ss >> vertexCount >> ss >>triangleCount;
+		stream >> ss >>  ss >>ss >> ss;
+
+		vector<core::math::Vertex> vertices;
+		for (int i = 0; i < vertexCount; ++i)
+		{
+			float vertex[6];
+			stream >> vertex[0] >> vertex[1] >> vertex[2] >> vertex[3] >> vertex[4] >> vertex[5];
+
+			core::math::Vertex v;
+			v.pos = core::math::Vector3(vertex[0], vertex[1], vertex[2]);
+			v.color = core::math::Vector4(vertex[3], vertex[4], vertex[5], 0);
+			
+			vertices.push_back(v);			
+		}
+		stream >> ss  >> ss >> ss;
+
+		vector<uint16_t> indecses;
+		for (int i = 0; i < triangleCount; ++i)
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				int ind;
+				stream >> ind;
+				indecses.push_back(ind);
+			}
+		}
 
 		return Model();
 	}
