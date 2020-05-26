2d41faca90f0ce070de73d0a4a9d620c51209ec2
remiznik <remiznik@gmail.com>
2020-05-26 23:26:58 +0300

    start working on vm

--- src/script_system/vm/vm.cpp
+++ src/script_system/vm/vm.cpp
@@ -1,8 +1,56 @@
 #include "vm.h"
+#include "vm_value.h"
 
 #include "common.h"
 
+#include <stdio.h>
 
 namespace script_system {
-namespace vm {}
+namespace vm {
+VM vm;
+void initVM() 
+{
+
+}
+
+void finiVM()
+{
+
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE() ]);
+    
+    for(;;)
+    {
+        uint8_t inistruction;
+        switch (inistruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            printValue(constant);
+            printf("\n");
+        }
+        case OP_RETURN:
+            return INTERPRET_OK;
+            break;
+        
+        default:
+            break;
+        }
+    }
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}   
+
+}
 }
--- src/script_system/vm/vm.h
+++ src/script_system/vm/vm.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include "chunk.h"
+
 namespace script_system {
 namespace vm {
+typedef struct vm
+{
+    Chunk* chunk;
+    uint8_t* ip;
+} VM;
+
+typedef enum {            
+  INTERPRET_OK,           
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR 
+} InterpretResult;   
+
+
+void initVM();
+void freeVM();
+
+InterpretResult interpret(Chunk* chunk);
 	
 }
 }
