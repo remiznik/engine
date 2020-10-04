62a0f566cef7c19bbf8f7047f721b2b2bdc26287
remiznik <remiznik@gmail.com>
2020-10-04 14:51:57 +0300

    remove old interpreter

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "script_system/script.h"
 #include "graphics/scene.h"
 #include "graphics/rectangle.h"
 
@@ -6,43 +5,28 @@
 #include <memory>
 #include <iostream>
 
-class Output : public core::Callable
-{
-public:
-
-	core::Value call(const vector<core::Value>& args) override
-	{
-		std::cout << args[0].to<string>() << std::endl;
-		return core::Value();
-	}
-
-	string toString() const  override
-	{
-		return "Output";
-	}
-};
 
 int  main()
 {
-	script_system::Script script;
-	script.registreFunction("output", makeShared<Output>());
-
-	script.run("../res/scripts/test.scr");
-	engine::Scene scene;
-
-	auto rectangle = std::make_shared<engine::Rectangle>();
-	scene.addShape(rectangle);		
-	
-	float lastTime = 0;
-	utils::FpsLock lock(20);
-
-	while (true)
-	{
-		if (!scene.draw())
-			break;
-		
-		lock.update();
-	}
+//	script_system::Script script;
+//	script.registreFunction("output", makeShared<Output>());
+//
+//	script.run("../res/scripts/test.scr");
+//	engine::Scene scene;
+//
+//	auto rectangle = std::make_shared<engine::Rectangle>();
+//	scene.addShape(rectangle);		
+//	
+//	float lastTime = 0;
+//	utils::FpsLock lock(20);
+//
+//	while (true)
+//	{
+//		if (!scene.draw())
+//			break;
+//		
+//		lock.update();
+//	}
 
 	return 0;
 }
