2cd7780a18894e094cc1fd42154cad2329e099a5
remiznik <remiznik@gmail.ocm>
2020-10-05 11:59:33 +0300

    fix Value macros for build in Visual Studio 2019

--- src/script_system/vm/vm_value.h
+++ src/script_system/vm/vm_value.h
@@ -12,22 +12,37 @@ namespace vm {
   		VAL_NUMBER,             
 	} ValueType; 
 
-	typedef struct {  
-  		ValueType type; 
-  		union {         
-    		bool boolean; 
-    		double number;
-  		} as; 
-	} Value;	
+    struct Value
+    {
+        ValueType type;
+        union {
+            bool boolean;
+            double number;
+        } as;
+
+        Value() : type(ValueType::VAL_NIL)
+        {
+            as.number = 0;
+        }
+
+        Value(bool boolean) : type(ValueType::VAL_BOOL)
+        {
+            as.boolean = boolean;
+        }
+        Value(double number) : type(ValueType::VAL_NUMBER)
+        {
+            as.number = number;
+        }
+    };
 	
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)  
 #define IS_NIL(value)     ((value).type == VAL_NIL)   
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
 #define AS_BOOL(value)    ((value).as.boolean)                       
 #define AS_NUMBER(value)  ((value).as.number) 
-#define BOOL_VAL(value)   ((Value){ VAL_BOOL, { .boolean = value } }) 
-#define NIL_VAL           ((Value){ VAL_NIL, { .number = 0 } })       
-#define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
+#define BOOL_VAL(value)   (Value(value)) 
+#define NIL_VAL           (Value())       
+#define NUMBER_VAL(value) (Value(value))
 
     typedef struct vm_value
     {
